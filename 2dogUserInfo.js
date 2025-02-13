9acead742690d1a2abae601557e4c969711b2eea
Acres <acres@outlook.com>
2025-02-13 22:39:42 +0800

    Remove clear user list button and related functionality from 2dog script

--- 2dogUserInfo.js
+++ 2dogUserInfo.js
@@ -58,11 +58,6 @@ function processResponse() {
   return $response.body;
 }
 
-function handleClearUserList() {
-  chavy.setdata(KEY_USERS, '[]');
-  chavy.msg("清空用户列表", "成功", "用户列表已清空。");
-}
-
 function init() {
   isSurge = () => {
     return undefined === this.$httpClient ? false : true;
@@ -86,11 +81,7 @@ function init() {
   done = (value = {}) => {
     $done(value);
   };
-  // 检查是否点击了 "清空用户列表" 按钮
-  if (getdata('clear_user_list') === '清空') {
-    setdata('','clear_user_list'); // 重置按钮状态，防止重复触发
-    handleClearUserList();
-  }
+
   return { isSurge, isQuanX, msg, log, getdata, setdata, done };
 }
 
